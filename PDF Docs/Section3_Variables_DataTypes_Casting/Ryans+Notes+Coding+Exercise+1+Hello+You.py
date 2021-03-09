00b6a01c5a2296e7d5c7fdd61b33cac8d6f9eb9f
battosai-slayer864 <79171645+battosai-slayer864@users.noreply.github.com>
2021-03-09 10:45:20 -0600

    Add files via upload

---
+++ PDF Docs/Section3_Variables_DataTypes_Casting/Ryans+Notes+Coding+Exercise+1+Hello+You.py
@@ -0,0 +1,18 @@
+'''
+These notes were graciously provided by Ryan, a student who took
+this course. He is very active in the Q&A section and I wanted to
+thank him for taking the time to further enrich this course
+through his contributions - Derek Banas
+
+Hello You
+To solve this problem
+Don't use the input function in this code
+1. Create a Variable named my_name
+2. Assign your name to that variable
+3. Out the word Hello followed by your name
+'''
+# 1. my_name variable
+# 2. assigned to my name
+my_name = "your name"
+# 3. output Hello and my_name
+print("Hello", my_name)
---
+++ PDF Docs/Section3_Variables_DataTypes_Casting/Ryans+Notes+Coding+Exercise+2+Casting+and+Adding.py
@@ -0,0 +1,13 @@
+'''
+Casting and Adding
+To solve this problem
+Don't use the input function in this code
+1. Create 2 variables named num_1 and num_2
+2. Assign a number string to each of the variable (Ex. "5")
+Output the addition of those 2 variables using this format 1 + 2 = 3
+'''
+# Variables num_1 and num_2 and assign number strings 1 and 2
+num_1="1"
+num_2="2"
+# output addition as 1 + 2 = 3
+print(num_1,"+",num_2,"=",(int(num_1)+int(num_2)))
