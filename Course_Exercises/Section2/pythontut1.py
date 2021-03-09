0a22db20df07c40e0d62abde35f25633022a8086
battosai-slayer864 <79171645+battosai-slayer864@users.noreply.github.com>
2021-03-09 11:07:44 -0600

    Create pythontut1.py

---
+++ Course_Exercises/Section2/pythontut1.py
@@ -0,0 +1,28 @@
+# Variable naming rules
+# Start with letter or underscore
+# Good practice is using _ and not camelCase
+
+my_age = 30
+my_name = Mike
+
+print("Hello", my_name)
+
+''' We created the name variable and called it 
+after it printed "Hello" '''
+
+### STRING VARIABLE ###
+
+# The String variable must contain double quotes
+# Below is an example on how to print quoted statements within your string
+# It works bcs we use the "escape sequence" (See Regex) 
+
+string_1 = "\"This is a quote"\"
+
+'''
+Newline : \n
+Backslash : \\
+Single quote : \'
+Backspace : \b
+Tab : \t
+'''
+
