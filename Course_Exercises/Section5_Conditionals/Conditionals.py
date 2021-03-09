2e103aee167dc1b490b66b3e8096ba7f2d6761f1
battosai-slayer864 <79171645+battosai-slayer864@users.noreply.github.com>
2021-03-09 14:59:53 -0600

    Create Conditionals.py

---
+++ Course_Exercises/Section5_Conditionals/Conditionals.py
@@ -0,0 +1,9 @@
+# Conditional Operators
+# > : Greater than
+# < : Less than
+# >= : Greater than or equal to
+# <= : Less than or equal to
+# == : Equal to
+# != : Not equal to
+
+drink = input("Pick One (Coke or Pepsi) : ")
