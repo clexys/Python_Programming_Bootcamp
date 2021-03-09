8801b85639e18f645248a147ea73154122650884
battosai-slayer691 <vashtienasheldy@gmail.com>
2021-03-09 14:18:18 -0600

    Completed video section

--- Course_Exercises/Section4/Input_&Math_Functions.py
+++ Course_Exercises/Section4/Input_&Math_Functions.py
@@ -47,11 +47,11 @@ Then print this for example 5 miles equals 8.0467 kilometers
 
 #MyGuess
 
-miles = input("Enter the number of miles : ")
-miles = int(miles)
-kilometers = miles * (1.691)
+# miles = input("Enter the number of miles : ")
+# miles = int(miles)
+# kilometers = miles * (1.691)
 
-print("miles equals kilometers, (miles, kilometers)
+# print("miles equals kilometers, (miles, kilometers)
       
 
 #TODO : Find exercies to solve another time. Need more practic!!!
@@ -76,22 +76,44 @@ print("floor(4.4) = ", math.floor(4.4))
 print("fabs(-4.4) = ", math.fabs(-4.4))
 
 # Factorial = Multiply number in sequence e.g 1*2*3*4 etc
-print("factorial(4) = ", math.factorial(4))
+print("factorial(6) = ", math.factorial(6))
 
-# Return remainder of division
+# Returns remainder of division
 print("fmod(5,4) = ", math.fmod(5,4))
 
-# Receive a float and return an int
+# Receives a float and return an int
 print("trunc(4.2) = ", math.trunc(4.2))
 
 # Return x^y
-print("pow(2,2) = ", math.pow(2,2))
+print("pow(2,5) = ", math.pow(2,5))
 
 # Return the square root
-print("sqrt(4) = ", math.sqrt(4))
+print("sqrt(8) = ", math.sqrt(8))
 
+# Special values
+print("math.e = ", math.e)
+print("math.pi = ", math.pi)
 
-      
-      
-      
+# Return e^x
+print("exp(4) = ", math.factorial(4))
+
+# Return the natural logarithm e * e * e ~= 20 so log(20) tells
+# you that e^3 ~= 20
+print("log(20) = ", math.log(20))
+
+# You can define the base and 10^3 = 1000
+print("log(1000,10) = ", math.log(1000,10))
+
+# You can also use base 10 like this
+print("log10(1000) = ", math.log10(1000))
+
+# We have the following trig functions
+# sin, cos, tan, asin, acos, atan, atan2, asinh, acosh,
+# atanh, sinh, cosh, tanh
+# They follow this format
+print("sin(0) ", math.sin(0))
+
+# Convert radians to degrees and vice versa
+print("degrees(1.5708) = ", math.degrees(1.5708))
+print("radians(90) = ", math.radians(90))
       
