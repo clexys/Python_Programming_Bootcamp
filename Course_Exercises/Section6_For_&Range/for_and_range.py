ece45c569e0b0d6021faf57910b62cac9d47d144
battosai-slayer691 <vashtienasheldy@gmail.com>
2021-03-10 10:30:16 -0600

    Python problem> My solution

--- Course_Exercises/Section6_For_&Range/for_and_range.py
+++ Course_Exercises/Section6_For_&Range/for_and_range.py
@@ -1,3 +1,26 @@
 # FOR AND RANGE
 # Conditionally execute till not true
 
+# Looping thru a list. The syntax is:
+# for >> the function
+# in >> defines where it will be looping
+for i in [2,4,6,8,10]:
+    print("i =", i)
+
+
+# RANGE
+'''It creates a list starting at 0 and going all the way up
+   to the number we define but not including that number. 
+    For example'''
+
+for i in range (6):
+    print("i =", i)
+
+i = 6
+(i % 2) == 0
+print("Is 6 even? : ", ((i % 2) == 0))
+
+# PROBLEM
+
+# Print all the odd numbers from 1-20
+
