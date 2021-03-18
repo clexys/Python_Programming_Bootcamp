aba5a92fe513238f9d19242c6023425d2ae90b1a
battosai-slayer864 <79171645+battosai-slayer864@users.noreply.github.com>
2021-03-18 12:35:52 -0600

    Update functions.py

--- Course_Exercises/Section11_Functions/functions.py
+++ Course_Exercises/Section11_Functions/functions.py
@@ -4,7 +4,9 @@ def add_number (num 1, num 2)
     return num_1 + num_2
 print("12 + 6",add_number(12, 6)
 
-#       
+''' The below example shows that when a variable is defined inside a function, it
+will not be available outside of it. Here, the system will print Tom, regardless of it being defined
+as Mark before'''
       
 def change_name(name)
       name = "Mark"
