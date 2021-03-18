762ad3ec5ab50620a8ee852f4e1aab90ec278013
battosai-slayer864 <79171645+battosai-slayer864@users.noreply.github.com>
2021-03-18 12:33:20 -0600

    Update functions.py

--- Course_Exercises/Section11_Functions/functions.py
+++ Course_Exercises/Section11_Functions/functions.py
@@ -3,3 +3,12 @@
 def add_number (num 1, num 2)
     return num_1 + num_2
 print("12 + 6",add_number(12, 6)
+
+#       
+      
+def change_name(name)
+      name = "Mark"
+      
+name = "Tom"
+change_name(name)      
+print(name)
