4d2ef005ae346cf0dfe5a73bd332c66448629d75
battosai-slayer691 <vashtienasheldy@gmail.com>
2021-03-18 09:21:48 -0600

    Updating problem solution

--- Course_Exercises/Strings/strings.py
+++ Course_Exercises/Strings/strings.py
@@ -95,13 +95,16 @@ print("65 =", chr(65))
 # First, request the string to work with
 norm_string = input("Enter a string to hide in uppercase: ")
 # Here I define secret_string as "" bcs if I had assigned a value, it wouldn't print what I wanted
-#
+# bcs on line 105 I'm saying that it will be equal to the char converted to unicode
 secret_string = ""
 
 # Cycle through each character in the string
 for char in norm_string:
     # Store each character code in a new string
     # += is the same as secret_string = secret_string + whatever
+    # Here str(ord(char)) means
+    # str - Converts what's inside of the () into a string type
+    # ord - Converts "char" to Unicode characters
     secret_string += str(ord(char))
 
 print("Secret Message :", secret_string)
@@ -109,7 +112,7 @@ print("Secret Message :", secret_string)
 norm_string = ""
 
 # Cycle through each character code 2 at a time by incrementing by
-# 2 each time through since unicodes go from 65 to 90
+# 2 each time through since Unicodes go from 65 to 90
 for i in range(0, len(secret_string)-1, 2):
     # Get the 1st and 2nd for the 2 digit number
     char_code = secret_string[i] + secret_string[i+1]
@@ -118,12 +121,39 @@ for i in range(0, len(secret_string)-1, 2):
     norm_string += chr(int(char_code))
 print("Original Message :", norm_string)
 
-# 2nd Python Problem for you to Solve
+# 2nd Python Problem
 
-# Now if you solved the previous problem I have another for you
-# Make the above work with upper and lowercase letters by changing 2 lines of code
+# Make the above take input with upper and lowercase letters by changing 2 lines of code
 
 # Add these 2 changes
 
 secret_string += str(ord(char) - 23)
 norm_string += chr(int(char_code) + 23)
+
+# Had to take the examples from the course
+
+# Take in a word
+UCString = str(input("Please enter a word: "))
+# Set the Unicode string to nothing at first
+UNIString = str("")
+# Convert string to unicode
+# We are going to subtract 23 from the unicode to handle the lowercase letters that
+# turn the ord into a triple digit (i.e. a - z is 97 - 122, hence d-z is over 100)
+for i in UCString:
+	UNIString += str(ord(i) - 23)
+# Print the Unicode String as unicode
+print("Unicode String is:", UNIString)
+print("Note: All double digit values are 23 less than the real unicode values")
+# Sets the UPPEPRCASE word to nothing in order to reset it from the unicode
+UCString = str("")
+# Loops the Unicode back into the UPPERCASE word string 1 letter at a time
+# Needs to pull in 2 at a time in order to get both digits of the unicode (65 - 90 for A - Z)
+for i in range(0, len(UNIString) - 1, 2):
+    # char will be 2 digits
+    char_UNICODE = UNIString[i] + UNIString[i+1]
+    # Adds each char individually based on the 2 digit unicode
+    # We add the 23 back to the unicode value because we removed it earlier to
+    # make the char_UNICODE only pull 2 digit values
+    UCString += chr(int(char_UNICODE) + 23)
+# prints the original UCString
+print("The original word:", UCString)
