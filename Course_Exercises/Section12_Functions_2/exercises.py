355bea1133fe70fa8553484f8bc5a981f93b3ffb
battosai-slayer864 <malta691@hotmail.com>
2021-03-23 15:13:39 -0400

    Added the code lines for the exercise to new file. Will tackle it later after finishing today's class

---
+++ Course_Exercises/Section12_Functions_2/exercises.py
@@ -0,0 +1,5 @@
+# Now, let's try to use the same function by asking the input from the user
+
+num_1, num_2 = input("Enter the 2 numbers you want to mult and div : ".split())
+print("Hello ", num_1, num_2)
+def mult_div(num_1, num_2):
