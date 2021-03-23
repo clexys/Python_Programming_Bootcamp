29e60fb6448b99ee611383294743530d43755c8b
battosai-slayer864 <malta691@hotmail.com>
2021-03-23 16:42:06 -0400

    Updating first part of comments

--- Course_Exercises/Section12_Functions_2/list_of_primes.py
+++ Course_Exercises/Section12_Functions_2/list_of_primes.py
@@ -1,10 +1,18 @@
+# First, we create our function and pass it to the value of num
+# The for loop cycles from 2 to the value to check\
+# NOTE> The first part is really important bcs without it the res
+# of the code breaks
+
 def is_prime(num):
     for i in range(2, num):
+        # Below, if the division has no remainder, then it
+        # isn't a prime
+        # Waiting on Derek's response to comment this part
         if (num % i) == 0:
             return False
         return True
 
-
+# Here define another function to get the prime numbers
 def get_primes(max_number):
     list_of_primes = []
     for num_1 in range(2, max_number):
