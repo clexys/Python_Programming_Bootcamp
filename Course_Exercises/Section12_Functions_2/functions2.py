160355aa1e1133bbcff38b997aed1e0b3290a11e
battosai-slayer864 <malta691@hotmail.com>
2021-03-23 15:09:50 -0400

    Removed the code lines for the exercise and added to new file called exercises

--- Course_Exercises/Section12_Functions_2/functions2.py
+++ Course_Exercises/Section12_Functions_2/functions2.py
@@ -13,9 +13,3 @@ mult, divide = mult_div(5, 4)
 print("5 * 4 =", mult)
 print("5 / 4 =", divide)
 
-# Now, let's try to use the same function by asking the input from the user
-
-num_1, num_2 = input("Enter the 2 numbers you want to mult and div : ".split())
-print("Hello ", num_1, num_2)
-def mult_div(num_1, num_2):
-    num_1
