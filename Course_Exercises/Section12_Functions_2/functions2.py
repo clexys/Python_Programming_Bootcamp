66e6422d0561b79ad13fa8be263b5b4736d1b8c5
battosai-slayer864 <malta691@hotmail.com>
2021-03-23 15:50:15 -0400

    Program to Return a List of Primes

--- Course_Exercises/Section12_Functions_2/functions2.py
+++ Course_Exercises/Section12_Functions_2/functions2.py
@@ -13,3 +13,23 @@ mult, divide = mult_div(5, 4)
 print("5 * 4 =", mult)
 print("5 / 4 =", divide)
 
+
+def is_prime(num):
+    for i in range(2, num):
+        if (num % i) == 0:
+            return False
+        return True
+
+
+def get_primes(max_number):
+    list_of_primes = []
+    for num_1 in range(2, max_number):
+        if is_prime(num_1):
+            list_of_primes.append(num_1)
+    return list_of_primes
+
+max_num_to_check = int(input("Search for Primes up to : "))
+
+list_of_primes = get_primes(max_num_to_check)
+for prime in list_of_primes:
+    print(prime)
