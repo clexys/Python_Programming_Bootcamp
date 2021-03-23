1e8a096780d750c209a8060f27ef45a704a0c41e
battosai-slayer864 <malta691@hotmail.com>
2021-03-23 15:08:44 -0400

    Fixed error by putting split inside the () at line 18 but got split output. Will pause the exercise and work it later to continue with class and create different file to work it separately

--- Course_Exercises/Section12_Functions_2/functions2.py
+++ Course_Exercises/Section12_Functions_2/functions2.py
@@ -15,5 +15,7 @@ print("5 / 4 =", divide)
 
 # Now, let's try to use the same function by asking the input from the user
 
-num_1, num_2 = input("Enter the 2 numbers you want to mult and div : ").split()
+num_1, num_2 = input("Enter the 2 numbers you want to mult and div : ".split())
+print("Hello ", num_1, num_2)
 def mult_div(num_1, num_2):
+    num_1
