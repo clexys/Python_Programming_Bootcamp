a5665bc38f87b6ed24b1cd2cdeb6ac20199ebbec
battosai-slayer864 <malta691@hotmail.com>
2021-03-24 15:05:28 -0400

    Adding the actual solution. Forgot to import random and I missed the append method at the beginning

--- Course_Exercises/Section13_Lists_1/lists.py
+++ Course_Exercises/Section13_Lists_1/lists.py
@@ -61,7 +61,93 @@ first3.append("Another")
 
 # MY GUESS
 
-my_list = []
-for my in range(5):
-    my_list(random.randrange(1,9))
+# my_list = []
+# for my in range(5):
+    # my_list(random.randrange(1, 9))
 
+# ACTUAL SOLUTION
+
+import random
+num_list = []
+for i in range(5):
+    num_list.append(random.randrange(1, 9))
+print(num_list)
+
+# Bubble Sort
+
+# The Bubble sort is a way to sort a list. It works this way :
+# 1. An outer loop decreases in size each time
+# 2. The goal is to have the largest number at the end of the list when the outer loop completes 1 cycle
+# 3. The inner loop starts comparing indexes at the beginning of the loop
+# 4. Check if list[Index] > list[Index + 1]
+# 5. If so swap the index values
+# 6. When the inner loop completes the largest number is at the end of the list
+# 7. Decrement the outer loop by 1
+
+# Create the value that will decrement for the outer loop
+# Its value is the last index in the list
+i = len(num_list) - 1
+while i > 1:
+    j = 0
+
+    while j < i:
+        # Tracks the comparison of index values
+        print("\nIs {} > {}".format(num_list[j], num_list[j+1]))
+        print()
+
+        # If the value on the left is bigger switch values
+        if num_list[j] > num_list[j+1]:
+            print("Switch")
+
+            temp = num_list[j]
+            num_list[j] = num_list[j + 1]
+            num_list[j + 1] = temp
+        else:
+            print("Don't Switch")
+
+        j += 1
+
+        # Track changes to the list
+        for k in num_list:
+            print(k, end=", ")
+        print()
+    print("END OF ROUND")
+
+    i -= 1
+
+for k in num_list:
+    print(k, end=", ")
+print()
+
+# More List Functions
+
+import random
+num_list = []
+for i in range(5):
+    num_list.append(random.randrange(1, 9))
+
+# Sort a list
+num_list.sort()
+
+# Reverse a list
+num_list.reverse()
+for k in num_list:
+    print(k, end=", ")
+print()
+
+# Insert value at index insert(index, value)
+num_list.insert(5, 10)
+
+# Delete first occurrence of value
+num_list.remove(10)
+
+for k in num_list:
+    print(k, end=", ")
+print()
+
+# Remove item at index
+num_list.pop(2)
+
+for k in num_list:
+    print(k, end=", ")
+print()
