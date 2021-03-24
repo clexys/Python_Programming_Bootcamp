61c8d9da3f59bbdfb2960431f4ec0b802fdce055
battosai-slayer864 <malta691@hotmail.com>
2021-03-24 14:08:42 -0400

    Updating comments for lists.py functions

--- Course_Exercises/Section13_Lists_1/lists.py
+++ Course_Exercises/Section13_Lists_1/lists.py
@@ -1,18 +1,22 @@
-# With lists we can refer to groups of data with 1 name
+# LISTS
+
+# With lists we refer to groups of data with 1 name
 # Each item in the list corresponds to a number (index) just like how people have identification numbers
 # By default the 1st item in a list has the index 0.
 
-# Ex. [0 : "string"] [1 : 1.234] [2 : 28] [3 : "c"]
-#
-# Python lists can grow in size and can contain data of any type
-# An awesome thing about lists is that you can use many of the same functions with them that you used with strings
+# In Python, lists can grow in size and can contain data of any type
+# You can also use many of the same functions with them that you used with strings
 
 rand_list = ["string", 1.234, 28]
+
+# rand_list is the list name
+# MUST have []
+# Data inside must be separated by a comma
 
 # Create a list with range
 one_to_ten = list(range(10))
 
-# Combine lists
+# List can be combined
 rand_list = rand_list + one_to_ten
 
 # Get the 1st item with an index
@@ -22,6 +26,7 @@ print(rand_list[0])
 print("List Length :", len(rand_list))
 
 # Slice a list to get 1st 3 items
+# This is how you get slices with strings too
 first3 = rand_list[0:3]
 
 # Cycle through the list and print the index
@@ -29,6 +34,7 @@ for i in first3:
     print("{} : {}".format(first3.index(i), i))
 
 # You can repeat a list item with *
+# The number after the * is the amount of time the item will repeat
 print(first3[0] * 3)
 
 # You can see if a list contains an item
