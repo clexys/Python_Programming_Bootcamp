a21ee3a387c9cd4b8870b2a0cc9fb18ee9f63dd5
battosai-slayer864 <malta691@hotmail.com>
2021-03-24 10:11:32 -0400

    Added missing input about the rest of the lesson. Forgot to pushed it from PyCharm yesterday

--- Course_Exercises/Section12_Functions_2/functions2.py
+++ Course_Exercises/Section12_Functions_2/functions2.py
@@ -58,3 +58,64 @@ def sum_all(*args):
     return sum_1
 
 print("Sum :", sum_all(1, 2, 3, 4))
+
+# FUNCTIONS TO CALCULATE AREAS OF DIFFERENT TYPES OF SHAPES
+
+# Here we will route to different functions depending on what type of shape we want to get the area for
+# We will also use a main function for the first time
+import math
+
+# This routes to the correct area function
+# The name of the value passed doesn't have to match
+def get_area(shape):
+
+    # Switch to lowercase for easy comparison
+    shape = shape.lower()
+
+    if shape == "rectangle":
+        rectangle_area()
+    elif shape == "circle":
+        circle_area()
+    else:
+        print("Please enter rectangle or circle")
+
+# Create function that calculates the rectangle area
+def rectangle_area():
+    length = float(input("Enter the length : "))
+    width = float(input("Enter the width : "))
+
+    area = length * width
+
+    print("The area of the rectangle is", area)
+
+
+# Create function that calculates the circle area
+def circle_area():
+    radius = float(input("Enter the radius : "))
+
+    area = math.pi * (math.pow(radius, 2))
+
+    # Format the output to 2 decimal places
+    print("The area of the circle is {:.2f}".format(area))
+
+# We often place our main programming logic in a function called main
+# We create it this way
+def main():
+
+    # Our program will calculate the area for rectangles or circles
+
+    # Without functions we'd have to create a giant list of ifs, elifs
+
+    # Ask the user what shape they have
+    shape_type = input("Get area for what shape : ")
+
+    # Call a function that will route to the correct function
+    get_area(shape_type)
+
+    # Because of functions it is very easy to see what is happening
+    # For more detail just refer to the very short specific functions
+
+# All of the function definitions are ignored and this calls for main()
+# to execute when the program starts
+
+main()
