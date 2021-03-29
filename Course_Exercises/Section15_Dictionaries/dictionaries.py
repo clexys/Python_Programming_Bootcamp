2528f853f97390b564b8e5b251771718e31ed856
battosai-slayer691 <vashtienasheldy@gmail.com>
2021-03-29 10:11:11 -0600

    Creating first dictionary and commenting on dictionaries structure

--- Course_Exercises/Section15_Dictionaries/dictionaries.py
+++ Course_Exercises/Section15_Dictionaries/dictionaries.py
@@ -3,3 +3,11 @@
 #Unlike lists, they use pair of key - values. Structure is 
 # fName : "Mike" >> fName is the key; "Mike" is the value
 
+mike_dict = {"first_name": "Mike", "last_name", "Altamirano",
+            "phone":"58496194"}
+# first we define the name mike_dict =
+# Then, we put everything insie {} and use double quotations for the key and the value
+# key and value MUST be separated by a " : "
+# To get the info on screen, we use print. We put the string text we want to show
+# Then, we call our dict and within " [] " we call the key we want to use
+print("My Name :", mike_dict["first_name"])
