dcc7d43a2fca723667846d1b57d7fa3480570f5b
battosai-slayer864 <malta691@hotmail.com>
2021-04-13 08:24:17 -0600

    Understanding the process behind a recursive function and commenting every part of it.

--- Course_Exercises/Section16_Recursive_Functions/recursive_functions.py
+++ Course_Exercises/Section16_Recursive_Functions/recursive_functions.py
@@ -1,8 +1,18 @@
 ''' Basically, a recursive function is that calls itself.
-It MUST contain to important parts > A recurrance relation and a termination condtion
-This last one, IS IMPERATIVE. Without a termination condtion, we will have an infinite loop 
+It MUST contain to important parts > A recurrence relation and a termination condition
+This last one, IS IMPERATIVE. Without a termination condition, we will have an infinite loop
 (remember the while True?, well. IT'S THE SAME)
 
 Also, Recursive Functions help us solve multiple problems with simple calculations repetitively, 
-instead of writing a big block of code.
+instead of writing a big block of code. '''
 
+# We will calculate the factorial of 4 and see the code behind that operation
+# function 4! = 3 * 2 * 1
+
+# We create our factorial function
+def factorial(num):
+    if num <= 1:
+        return 1
+    else:
+        result = num * factorial(num - 1)
+        return result
