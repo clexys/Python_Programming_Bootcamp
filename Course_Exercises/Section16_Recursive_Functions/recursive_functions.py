f813ecd5cbbe119e40c6d1c7ceb94064d5d10efa
battosai-slayer864 <79171645+battosai-slayer864@users.noreply.github.com>
2021-04-13 07:41:50 -0600

    Create recursive_functions.py

---
+++ Course_Exercises/Section16_Recursive_Functions/recursive_functions.py
@@ -0,0 +1,8 @@
+''' Basically, a recursive function is that calls itself.
+It MUST contain to important parts > A recurrance relation and a termination condtion
+This last one, IS IMPERATIVE. Without a termination condtion, we will have an infinite loop 
+(remember the while True?, well. IT'S THE SAME)
+
+Also, Recursive Functions help us solve multiple problems with simple calculations repetitively, 
+instead of writing a big block of code.
+
